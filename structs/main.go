7d7963f27839f38c0a16be83fd3fbae5246e0ff6
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-09-06 10:54:34 -0500

    structs 1

--- structs/main.go
+++ structs/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	//can declare key or it will go by struct order.
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// fmt.Println(alex)
+	//another way to assign a struct go will default 0 value.
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
 }
