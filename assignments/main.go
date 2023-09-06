9fb2ec4ea478b6de38f6368ef288b1e488ae99c8
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-09-06 09:17:15 -0500

    added yelling

--- assignments/main.go
+++ assignments/main.go
@@ -6,9 +6,9 @@ func main() {
 	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	for _, value := range nums {
 		if value%2 == 0 {
-			fmt.Println("EVEN")
+			fmt.Println(value, "is EVEN!")
 		} else {
-			fmt.Println("ODD")
+			fmt.Println(value, "is ODD!")
 		}
 	}
 }
