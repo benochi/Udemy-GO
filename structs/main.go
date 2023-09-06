6e13b69292c871b1dfd634e0f0bf21337328a00e
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-09-06 11:01:25 -0500

    some struct info

--- structs/main.go
+++ structs/main.go
@@ -10,14 +10,14 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Roberts",
-		contact: contactInfo{
+		contactInfo{
 			email:   "123@12334.com",
 			zipCode: 12345,
 		},
