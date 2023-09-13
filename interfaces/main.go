78237dbc65a421b8fcfa5819c77bce922c2f2e69
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-09-13 10:15:13 -0500

    added russianbot

--- interfaces/main.go
+++ interfaces/main.go
@@ -3,33 +3,36 @@ package main
 import "fmt"
 
 //Interfaces are trying to make reusing code easier.
-
+type bot interface {
+	getGreeting() string
+}
 type englishBot struct{}
 type spanishBot struct{}
+type russianBot struct{}
 
 func main() {
 	eb := englishBot{}
-	// sb := spanishBot{}
+	sb := spanishBot{}
+	rb := russianBot{}
 
 	printGreeting(eb)
-	// printGreeting(sb)
+	printGreeting(sb)
+	printGreeting(rb)
 }
-
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
-
-// func printGreeting(sb spanishBot) {
-// 	fmt.Println(sb.getGreeting())
-// }
 
 //could put these on structs
 //can remove receiver value and only leave type if not using variable IE 'eb/sb'
 func (englishBot) getGreeting() string {
-	//Very custom logic for generating English greeting
 	return "Hi There!"
 }
 
-// func (spanishBot) getGreeting() string {
-// 	return "Hola!"
-// }
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+func (russianBot) getGreeting() string {
+	return "привет!"
+}
