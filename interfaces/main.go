8dd84d1d973ee48454f7997fd564005894c06748
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-09-13 09:47:21 -0500

    start interfaces

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+//Interfaces are trying to make reusing code easier.
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	fmt.Println("Duh")
+}
+
+//could put these on structs
+func (eb englishBot) getGreeting() string {
+	//Very custom logic for generating English greeting
+	return "Hi There!"
+}
+
+func (sb spanishBot) getGreeting() string {
+	//Very custom logic for generating spanish greeting
+	return "Hola!"
 }
