df333807cf97086bdd13a9d4b818d9e8a906db9a
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-09-14 14:30:30 -0500

    http cluster

--- http/main.go
+++ http/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type httpBody interface{
+	*Response struct
+}
+
 func main() {
 
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	//for Go, resp.body doesn't work.
+	fmt.Println(resp)
 }
