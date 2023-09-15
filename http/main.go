83cfddcf9f754bd909bff2463ce847e20db0b772
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-09-15 16:40:19 -0500

    os writer interface

--- http/main.go
+++ http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -14,7 +15,8 @@ func main() {
 		os.Exit(1)
 	}
 	//for Go, resp.body doesn't work. create byteslice with make
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+	io.Copy(os.Stdout, resp.Body)
 }
