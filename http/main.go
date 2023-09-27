a4aa6ccbd47a3131179d4cea4b09f9add6ad79ad
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-09-27 09:38:24 -0500

    updated bytewriter

--- http/main.go
+++ http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 
 	resp, err := http.Get("http://google.com")
@@ -18,5 +20,16 @@ func main() {
 	// bs := make([]byte, 99999)
 	// resp.Body.Read(bs)
 	// fmt.Println(string(bs))
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+	//os.Stdout is a rwriter function repalces by lw
+	io.Copy(lw, resp.Body)
+}
+
+// implementing the writer interface, becuase writer has a function called Write.
+func (logWriter) Write(bs []byte) (int, error) {
+	//interfaces don't enforce correct code so you can mess this up.
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	//return int and err, but make sure valuies are correct per documentation.
+	return len(bs), nil
 }
