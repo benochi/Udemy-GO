eeae1597f9fcd547a90cc9f170ad88f58084fa60
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-10-03 13:34:20 -0500

    add channel

--- channels/main.go
+++ channels/main.go
@@ -19,17 +19,24 @@ func main() {
 		"http://youtube.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
 
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down.")
+		c <- "Might be down, I think."
 		return
 	}
+
 	fmt.Println(link, "is up and running.")
+	c <- "Yep, it's up."
 }
